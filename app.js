ce36b5134e5fe4794692446efbba8a3ddc58f5db
Rudzix66 <kubadabrowa14@gmail.com>
2022-03-23 16:36:03 +0100

    localstoreage alpha

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-const statusBtn = document.querySelectorAll('.status-btn');
+( function () {
+    const statusBtn = document.querySelectorAll('.status-btn');
+
+    statusBtn.forEach(btn => btn.addEventListener('click', () => {
+        document.querySelector('body').addEventListener('click', () => {
+            btn.classList.remove('active')
+        })
+
+        btn.classList.toggle('active')
+        localStorage.setItem('userStatus', btn.dataset.status)
+
+        const userStatus = localStorage.getItem('userStatus');
+
+        if(userStatus === 'status-1' && btn.dataset.status === 'status-1') {
+            btn.classList.add('active')
+        }
+        if(userStatus === 'status-2' && btn.dataset.status === 'status-2') {
+            btn.classList.add('active')
+        }
+        if(userStatus === 'status-3' && btn.dataset.status === 'status-3') {
+            btn.classList.add('active')
+        }
+
+    }))
 
 
-statusBtn.forEach(btn => btn.addEventListener('click', () => {
-    document,querySelector('body').addEventListener('click', () => {
-        btn.classList.remove('active')
-    })
-    btn.classList.toggle('active')
-}))
+} )();
