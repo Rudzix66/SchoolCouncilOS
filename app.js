3b5fefa33820370bd2f10c330c130b5d2a9c4213
Rudzix66 <kubadabrowa14@gmail.com>
2022-03-23 20:09:54 +0100

    mobile test

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 ( function () {
     const statusBtn = document.querySelectorAll('.status-btn');
+    statusBtn.forEach(btn => btn.classList.remove('active'))
+
+    const userStatus = localStorage.getItem('userStatus');
 
     statusBtn.forEach(btn => btn.addEventListener('click', () => {
+
+        if(!localStorage.getItem('userStatus')) {
+            localStorage.setItem('userStatus', btn.dataset.status)
+
+        } else if(localStorage.getItem('userStatus')) {
+            if(userStatus === btn.dataset.status) {
+                btn.classList.add('active')
+            }      
+        }
+
         document.querySelector('body').addEventListener('click', () => {
             btn.classList.remove('active')
+            localStorage.setItem('userStatus', btn.dataset.status)
         })
 
         btn.classList.toggle('active')
-        localStorage.setItem('userStatus', btn.dataset.status)
-
-        const userStatus = localStorage.getItem('userStatus');
-
-        if(userStatus === 'status-1' && btn.dataset.status === 'status-1') {
-            btn.classList.add('active')
-        }
-        if(userStatus === 'status-2' && btn.dataset.status === 'status-2') {
-            btn.classList.add('active')
-        }
-        if(userStatus === 'status-3' && btn.dataset.status === 'status-3') {
-            btn.classList.add('active')
-        }
-
     }))
 
 
