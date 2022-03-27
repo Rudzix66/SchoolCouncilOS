cc69feaa21abae008a62b2c980debd27804fd9fa
Rudzix66 <kubadabrowa14@gmail.com>
2022-03-27 22:26:07 +0200

    commit

--- app.js
+++ app.js
@@ -1,40 +1,37 @@
-'use strict';
+"use strict";
 
-( function () {
-    const statusBtn = document.querySelectorAll('.status-btn');
+(function () {
+  const statusBtn = document.querySelectorAll(".status-btn");
 
-    restoreActiveBtn()
+  restoreActiveBtn();
 
-    statusBtn.forEach(btn => btn.addEventListener('click', () => {
+  statusBtn.forEach((btn) =>
+    btn.addEventListener("click", () => {
+      // remove the active class from all buttons to avoid bugs
+      statusBtn.forEach((btn) => btn.classList.remove("active"));
 
-        // remove the active class from all buttons to avoid bugs
-        statusBtn.forEach(btn => btn.classList.remove('active'));
-        
-        // add the active class to the clicked button
-        btn.classList.add('active')
-    
-        // store the button id in local storage as buttonID item
-        if(btn.classList.contains('active')) {
-            rememberButton(btn);
-        };
-        // to remove the active class from buttons when body clicked
-        // usuwam klasy active z buttonów kiedy kliknięto body
-        document.querySelector('body').addEventListener('click', () => {
-            btn.classList.remove('active')
-        })
-    }))
+      // add the active class to the clicked button
+      btn.classList.add("active");
 
-} )();
+      // store the button id in local storage as buttonID item
+      if (btn.classList.contains("active")) {
+        rememberButton(btn);
+      }
+      // to remove the active class from buttons when body clicked
+      // usuwam klasy active z buttonów kiedy kliknięto body
+      document.querySelector("body").addEventListener("click", () => {
+        btn.classList.remove("active");
+      });
+    })
+  );
+})();
 
 function rememberButton(button) {
-	localStorage.setItem("buttonID", button.id);
+  localStorage.setItem("buttonID", button.id);
 }
 
-
 function restoreActiveBtn() {
-    const buttonID = localStorage.getItem("buttonID");
-    const activeBtn = document.getElementById(buttonID);
-    console.log(activeBtn.classList.contains("active"))
-    activeBtn.classList.add("active");
-    console.log(activeBtn.classList.contains("active"));
+  const buttonID = localStorage.getItem("buttonID");
+  const activeBtn = document.getElementById(buttonID);
+  activeBtn.classList.add("active");
 }
